e213b15e8f4bc0d8c42462d913bbbca9e3b57dc8
Jonatas Rodrigues <jhonatas.fender@gmail.com>
2020-04-12 21:45:17 -0300

    corrigindo o responsivo e aplicando melhor o meta tags

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,10 +2,11 @@ import { Controllers } from "./controllers/controllers";
 
 export class Bootstrap {
   static init() {
+
     $.getScript(
       "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
     ).done((script, textStatus) => {
-      $("document").ready(function() {
+      $("document").ready(function () {
         MathJax.typesetPromise()
           .then(() => {
             // modify the DOM here
